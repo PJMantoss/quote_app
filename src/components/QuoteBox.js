9570f27480ea5dd0dc28cdaca5a1cdbec16e8390
pjmantoss <joelptoss@gmail.com>
2020-03-23 17:32:47 +0100

    update QuoteBox.js

--- src/components/QuoteBox.js
+++ src/components/QuoteBox.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-export default function QuoteBox() {
+export default function QuoteBox({quote, author}) {
     return (
         <div>
-            
+            <React.Fragment>
+                <div id="text"><p>{quote}</p></div>
+                <div id="author"><h5>{author}</h5></div>
+            </React.Fragment>
         </div>
     )
 }
