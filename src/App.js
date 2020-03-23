b8d31ca92b5db0db5b07c66537c5719977ded31c
pjmantoss <joelptoss@gmail.com>
2020-03-23 16:49:13 +0100

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      quote: '',
+      author: ''
+    }
+  }
+
+  componentDidMount(){
+    this.getQuote();
+  }
+
+  getQuote(){}
+
+
   render() {
     return (
       <div className="App">
