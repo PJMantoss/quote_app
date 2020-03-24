8436e001732717bbc818c274826fbc17d43b272d
pjmantoss <joelptoss@gmail.com>
2020-03-24 15:17:02 +0100

    update Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 export default function Button({onClick, title}) {
     return (
-        <div>
-            <button className="button" id="new-quote" onClick={onClick}>{title}</button>
-        </div>
+        
+            <button className="button" id="new-quote" onClick={onClick}>
+                {title}
+            </button>
+        
     )
 }
